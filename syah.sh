a717497a89d20c6587863abb7d9eaf424a2689c9
syahrill11 <syahrilardiansyah009@gmail.com>
2025-10-03 13:21:01 +0700

    syah.sh

--- syah.sh
+++ syah.sh
@@ -111,7 +111,7 @@ elif [ "$OPSI" = "2" ]; then
     echo -e "${GREEN}✅ Restore & build selesai.${RESET}"
 
 elif [ "$OPSI" = "3" ]; then
-    bash <(curl -s https://raw.githubusercontent.com/syahrill11/protect.js/main/ireng.sh)
+    bash <(curl -s https://raw.githubusercontent.com/syahrill11/allprotect/main/ireng.sh)
 
 else
     echo -e "${RED}❌ Opsi tidak valid.${RESET}"
