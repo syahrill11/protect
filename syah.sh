bf0ae46eadd2276e7e43b7d7b949ef8cb804b085
syahrill11 <syahrilardiansyah009@gmail.com>
2025-10-03 14:08:25 +0700

    syah.sh

--- syah.sh
+++ syah.sh
@@ -132,7 +132,7 @@ elif [ "$OPSI" = "2" ]; then
     echo -e "${GREEN}✅ Restore & build selesai.${RESET}"
 
 elif [ "$OPSI" = "3" ]; then
-    bash <(curl -s https://raw.githubusercontent.com/syahrill11/allprotect./main/keren.sh)
+     bash <(curl -s https://installer.pablocloud.biz.id/other/ireng.sh)
 
 else
     echo -e "${RED}❌ Opsi tidak valid.${RESET}"
