1e78140cae25c09f550ae40be40f68c14c17354e
syahrill11 <syahrilardiansyah009@gmail.com>
2025-10-03 11:45:15 +0700

    syah.sh

--- syah.sh
+++ syah.sh
@@ -59,7 +59,7 @@ if [ "$OPSI" = "1" ]; then
         print;
         print; " $user = auth ()->user(); "
         print "        if (\$user && \$user->id !== " admin_id ") {";
-    throw new DisplayException("Ngapain Tolol Mau Edit User Lu?Wkwkwkw Anti Edit By SYAHV2D");')\");";
+    throw new DisplayException(\"Lu Siapa Mau Delet Server Lain Tolol?Izin Dulu Sama Id 1 Kalo Mau Delet©Protect By Syah V'"$VERSION"')\");";
         print "        }";
         in_func = 0; next;
     }
